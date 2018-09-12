84466aa123281fdd3b182ad2f2f99764c3a34372
wsz1415554528 <s20161104602@mac22.local>
2018-09-12 11:32:47 +0800

    Initial Commit

---
+++ 1+100/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1+100
+//
+//  Created by s20161104602 on 18/9/12.
+//  Copyright © 2018年 s20161104602. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
