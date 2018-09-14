78b0c7ddc15fe9dd2ee9ad6bc6cd9b1597c4c6ea
wsz1415554528 <s20161104602@mac22.local>
2018-09-14 17:02:21 +0800

    second

--- 1+100/main.swift
+++ 1+100/main.swift
@@ -16,3 +16,43 @@ for i in 1...100
     sum = sum + i
 print(sum)
 }
+
+
+
+
+
+
+let www = true
+let sss = false
+if www && sss {
+print("weclcome")
+    
+}
+else {
+print("no,thank")
+
+
+}
+
+
+func optimizationPopSort(list: inout [Int])
+{
+let n = list.count
+for i in 0..<n-1
+{
+    var flag = 0
+    var j = 0
+    for _ in 0..<(n-1-i)
+   if list[j] > [list[j+1]
+   {
+    list[j] ^= list[j+1]
+    list[j+1] ^= list[j]
+    list[j] ^= list[j+1]
+    flag = 1
+    }
+    j += 1
+    }
+    if flag == 0 {
+        braek}
+}
+}
